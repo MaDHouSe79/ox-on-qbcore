06b30d81f54178db36dfe272c4cac8f23e66126f
MaDHouSe <madhouse1979@gmail.com>
2025-01-21 16:37:04 +0100

    Update convert.lua

--- ox-scripts/ox_inventory/modules/mysql/convert.lua
+++ ox-scripts/ox_inventory/modules/mysql/convert.lua
@@ -5,6 +5,19 @@ local function Print(arg)
 	print(('^3=================================================================\n^0%s\n^3=================================================================^0'):format(arg))
 end
 
+local function SplitStr(str, delimiter)
+    local result = {}
+    local from = 1
+    local delim_from, delim_to = string.find(str, delimiter, from)
+    while delim_from do
+        result[#result + 1] = string.sub(str, from, delim_from - 1)
+        from = delim_to + 1
+        delim_from, delim_to = string.find(str, delimiter, from)
+    end
+    result[#result + 1] = string.sub(str, from)
+    return result
+end
+
 local function Upgrade()
 	if started then
 		return warn('Data is already being converted, please wait..')
